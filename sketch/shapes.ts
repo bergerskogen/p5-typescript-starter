9d6d7b0313f8bb2212fee7070a5555d159269255
Pål Bergerskogen <pbe@genus.biz>
2019-08-21 13:25:10 +0200

    feat: add triangle shape

--- sketch/shapes.ts
+++ sketch/shapes.ts
@@ -17,20 +17,32 @@ class Shapes {
     const points = new Array<p5.Vector>();
     // A square is a bunch of vertices along straight lines
     // Top of square
-    for (var x = -size; x < size; x += 10) {
+    for (let x = -size; x < size; x += 10) {
       points.push(createVector(x, -size));
     }
     // Right side
-    for (var y = -size; y < size; y += 10) {
+    for (let y = -size; y < size; y += 10) {
       points.push(createVector(size, y));
     }
     // Bottom
-    for (var x = size; x > -size; x -= 10) {
+    for (let x = size; x > -size; x -= 10) {
       points.push(createVector(x, size));
     }
     // Left side
-    for (var y = size; y > -size; y -= 10) {
+    for (let y = size; y > -size; y -= 10) {
       points.push(createVector(-size, y));
+    }
+
+    return points;
+  }
+
+  static triangle(size: number): p5.Vector[] {
+    const points = new Array<p5.Vector>();
+
+    const tanAngle = tan(TWO_PI / 6);
+    const halfWidth = Math.round((size * sin(PI / 6)) / 10) * 10;
+    for (let x = -halfWidth; x <= halfWidth; x += 10) {
+      points.push(createVector(x, halfWidth - (halfWidth - abs(x)) * tanAngle));
     }
 
     return points;
@@ -44,13 +56,13 @@ class Shapes {
     radius2: number,
     npoints: number
   ): p5.Vector[] {
-    var angle = TWO_PI / npoints;
-    var halfAngle = angle / 2.0;
+    const angle = TWO_PI / npoints;
+    const halfAngle = angle / 2.0;
 
     const points = new Array<p5.Vector>();
     for (var a = 0; a < TWO_PI; a += angle) {
-      var sx = x + cos(a) * radius2;
-      var sy = y + sin(a) * radius2;
+      let sx = x + cos(a) * radius2;
+      let sy = y + sin(a) * radius2;
       points.push(createVector(sx, sy));
       sx = x + cos(a + halfAngle) * radius1;
       sy = y + sin(a + halfAngle) * radius1;
