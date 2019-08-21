1a241c541ec70c1bf11a609729041b599d8a12d9
Pål Bergerskogen <pbe@genus.biz>
2019-08-21 13:51:36 +0200

    feat: draw initial game board

--- sketch/game.ts
+++ sketch/game.ts
@@ -1,14 +1,28 @@
 class Game {
   tiles: number[];
   tileSize: number;
+  x: number;
+  y: number;
 
   setup(gameSize: number = 800) {
-    this.tileSize = Math.floor(gameSize / 16);
+    this.tileSize = Math.floor(gameSize / 4);
+    this.x = (windowWidth - gameSize) / 2;
+    this.y = (windowHeight - gameSize) / 2;
     this.tiles = new Array<number>(16);
     for (let i = 0; i < 16; i++) {
       this.tiles[i] = null;
     }
   }
 
-  draw() {}
+  draw() {
+    this.tiles.forEach((tileValue, index) => {
+      this.drawTile(tileValue, index);
+    });
+  }
+
+  private drawTile(tileValue: number, index: number) {
+    const x = this.x + this.tileSize * (index % 4);
+    const y = this.y + this.tileSize * Math.floor(index / 4);
+    rect(x, y, this.tileSize - 5, this.tileSize - 5);
+  }
 }
